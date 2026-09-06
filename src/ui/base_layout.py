2d3c97d7b02419856ad2b9485eed62ed0368f984
Aman05cody <amanprasadbari05@gmail.com>
2026-09-06 17:03:45 +0530

    Style

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -1,144 +1,112 @@
 import streamlit as st
 
 
+
 def style_background_home():
 
     st.markdown("""
         <style>
 
-            .stApp {
-                background: #5865F2 !important;
-            }
+                .stApp {
+                    background: #5865F2 !important;
+                }
 
-            .stApp div[data-testid="stColumn"] {
-                background-color: #E0E3FF !important;
-                padding: 2.5rem !important;
-                border-radius: 5rem !important;
-            }
+                .stApp div[data-testid="stColumn"]{
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius: 5rem !important;
+                    }
+        </style>  
 
-        </style>
-    """, unsafe_allow_html=True)
-
+                """
+            ,unsafe_allow_html=True)
+    
 
 def style_background_dashboard():
 
     st.markdown("""
         <style>
 
-            .stApp {
-                background: #E0E3FF !important;
-            }
+                .stApp {
+                    background: #E0E3FF !important;
+                }
 
-        </style>
-    """, unsafe_allow_html=True)
+        </style>  
 
+                """
+            ,unsafe_allow_html=True)
+    
+
+    
 
 def style_base_layout():
-
+# asdasd
     st.markdown("""
         <style>
-
         @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
+                
+         /* Hide Top Bar of streamlit */
+                
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
+                
+            .block-container {
+                padding-top:1.5rem !important;    
+            }
 
-        /* Hide Top Bar of Streamlit */
+            h1 {
+                font-family: 'Climate Crisis', sans-serif !important;
+                font-size: 3.5rem !important;
+                line-height:1.1 1important;
+                margin-bottom:0rem !important;
+            }
+                
 
-        #MainMenu, footer, header {
-            visibility: hidden;
-        }
+            h2 {
+                font-family: 'Climate Crisis', sans-serif !important;
+                font-size: 2rem !important;
+                line-height:0.9 !important;
+                margin-bottom:0rem !important;
+            }
+                
+            h3, h4, p {
+                font-family: 'Outfit', sans-serif;    
+            }
+                
 
+            button{
+                border-radius: 1.5rem !important;
+                background-color: #5865F2 !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
 
-        .block-container {
-            padding-top: 1.5rem !important;
-        }
+            button[kind="secondary"]{
+                border-radius: 1.5rem !important;
+                background-color: #EB459E !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
 
+            button[kind="tertiary"]{
+                border-radius: 1.5rem !important;
+                background-color: black !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+                }
 
-        /* Main Headings */
+            button:hover{
+                transform :scale(1.05)}
+        </style>  
 
-        h1 {
-            font-family: 'Climate Crisis', sans-serif !important;
-            font-size: 3.5rem !important;
-            line-height: 1.1 !important;
-            margin-bottom: 0rem !important;
-            color: black !important;
-        }
-
-
-        h2 {
-            font-family: 'Climate Crisis', sans-serif !important;
-            font-size: 2rem !important;
-            line-height: 0.9 !important;
-            margin-bottom: 0rem !important;
-            color: black !important;
-        }
-
-
-        /* Normal Text */
-
-        h3, h4, p, label {
-            font-family: 'Outfit', sans-serif !important;
-            color: black !important;
-        }
-
-
-        /* Input Text */
-
-        input {
-            color: black !important;
-        }
-
-
-        /* Input Placeholder */
-
-        input::placeholder {
-            color: white !important;
-            opacity: 1 !important;
-        }
-
-
-        /* Streamlit Widget Labels */
-
-        [data-testid="stWidgetLabel"] p {
-            color: black !important;
-        }
-
-
-        /* Buttons */
-
-        button {
-            border-radius: 1.5rem !important;
-            background-color: #5865F2 !important;
-            color: white !important;
-            padding: 10px 20px !important;
-            border: none !important;
-            transition: transform 0.25s ease-in-out !important;
-        }
-
-
-        button[kind="secondary"] {
-            border-radius: 1.5rem !important;
-            background-color: #EB459E !important;
-            color: white !important;
-            padding: 10px 20px !important;
-            border: none !important;
-            transition: transform 0.25s ease-in-out !important;
-        }
-
-
-        button[kind="tertiary"] {
-            border-radius: 1.5rem !important;
-            background-color: black !important;
-            color: white !important;
-            padding: 10px 20px !important;
-            border: none !important;
-            transition: transform 0.25s ease-in-out !important;
-        }
-
-
-        button:hover {
-            transform: scale(1.05);
-        }
-
-        </style>
-    """, unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
